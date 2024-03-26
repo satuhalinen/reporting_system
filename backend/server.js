d68a847128c8c2e45d174cae074be7869e7e7c01
[satuhalinen] <satuh9@gmail.com>
2024-03-26 16:49:56 +0200

    add endpoint to fetch worklog data from db

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("db.sqlite3");
+
+app.get("/", (req, res) => {
+  db.all("SELECT * FROM worklog_worklog", (err, rows) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({ data: rows });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}.`);
+});
