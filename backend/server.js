752e9858de4862a6a82ce3dd6a40d9e48069b9ee
[satuhalinen] <satuh9@gmail.com>
2024-06-12 10:41:51 +0300

    change month and year to integer in SQL query

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.get("/monthly-working-hours/:year/:years_back", (req, res) => {
   year = year.toString();
   lastYear = lastYear.toString();
   db.all(
-    "SELECT strftime('%Y', date) AS year, strftime('%m', date) AS month, SUM(hours) AS total_hours FROM worklog_worklog WHERE (strftime('%Y', date) between ? AND ?) AND (deleted = 0) GROUP BY year, month ORDER BY year, month DESC",
+    "SELECT CAST(strftime('%Y', date) AS INTEGER) AS year, CAST(strftime('%m', date) AS INTEGER) AS month, CAST(SUM(hours) AS INTEGER) AS total_hours FROM worklog_worklog WHERE (strftime('%Y', date) between ? AND ?) AND (deleted = 0) GROUP BY year, month ORDER BY year, month DESC",
     [lastYear, year],
     (err, rows) => {
       if (err) {
