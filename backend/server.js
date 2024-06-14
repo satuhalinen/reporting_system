2703cf1f439df06fd75375c9d299c755893105b2
[satuhalinen] <satuh9@gmail.com>
2024-06-14 11:16:03 +0300

    rename table title variable, remove useless get from server.js and move Suorita button

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,6 @@ app.use(cors());
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("db.sqlite3");
 
-app.get("/", (req, res) => {
-  db.all("SELECT * FROM worklog_worklog", (err, rows) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({ data: rows });
-  });
-});
-
 app.get("/working-hours", (req, res) => {
   db.all(
     "SELECT tuntikirjaus_employee.firstname, tuntikirjaus_employee.lastname, worklog_worklog.hours FROM tuntikirjaus_employee JOIN worklog_worklog ON tuntikirjaus_employee.id = worklog_worklog.employee_id",
