16c4ef995b388042ef1f8db1e7b2af07d15b2e6e
[satuhalinen] <satuh9@gmail.com>
2024-06-28 14:42:21 +0300

    fix the startYear and endYear naming

--- backend/server.js
+++ backend/server.js
@@ -20,15 +20,15 @@ app.get("/working-hours", (req, res) => {
   );
 });
 
-app.get("/monthly-working-hours/:startYear/:years_back", (req, res) => {
-  let startYear = req.params.startYear;
+app.get("/monthly-working-hours/:endYear/:years_back", (req, res) => {
+  let endYear = req.params.endYear;
   const yearsBack = req.params.years_back;
-  let endYear = Number(startYear) - Number(yearsBack);
-  startYear = startYear.toString();
+  let startYear = Number(endYear) - Number(yearsBack);
   endYear = endYear.toString();
+  startYear = startYear.toString();
   db.all(
     "SELECT CAST(strftime('%Y', date) AS INTEGER) AS year, CAST(strftime('%m', date) AS INTEGER) AS month, SUM(hours) AS total_hours FROM worklog_worklog WHERE (strftime('%Y', date) between ? AND ?) AND (deleted = 0) GROUP BY year, month ORDER BY year, month",
-    [endYear, startYear],
+    [startYear, endYear],
     (err, rows) => {
       if (err) {
         res.status(400).json({ error: err.message });
