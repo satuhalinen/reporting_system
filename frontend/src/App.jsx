10b7d7ea7cb4ec1c1cb3b41b31d1d994937ad6a5
[satuhalinen] <satuh9@gmail.com>
2024-07-09 14:30:26 +0300

    add routing and jsx file for UserList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import CumulativeMonthlyWorkingHours from "./routes/CumulativeMonthlyWorkingHour
 import BillabilityMonthlyWorkingHours from "./routes/BillabilityMonthlyWorkingHours";
 import Salary from "./routes/Salary";
 import AddUser from "./routes/AddUser";
+import UserList from "./routes/UserList";
 
 const { Header, Content, Footer } = Layout;
 
@@ -39,6 +40,9 @@ const App = () => {
             defaultSelectedKeys={["2"]}
             style={{ flex: 1, minWidth: 0 }}
           />
+          <NavLink style={{ color: "white" }} to={`/user-list`}>
+            User list
+          </NavLink>
           <NavLink style={{ color: "white" }} to={`/add-user`}>
             Add user
           </NavLink>
@@ -105,6 +109,7 @@ const App = () => {
                 />
                 <Route path="salary" element={<Salary />} />
                 <Route path="add-user" element={<AddUser />} />
+                <Route path="user-list" element={<UserList />} />
               </Route>
             </Routes>
           </div>
---
+++ frontend/src/routes/UserList.jsx
@@ -0,0 +1,5 @@
+const UserList = () => {
+  return <h1>Here is the list of users!</h1>;
+};
+
+export default UserList;
