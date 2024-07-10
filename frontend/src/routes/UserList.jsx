11a213c0992ae578a1549b761378289a3db8b934
[satuhalinen] <satuh9@gmail.com>
2024-07-10 13:49:19 +0300

    make a table on user emails

--- frontend/src/routes/UserList.jsx
+++ frontend/src/routes/UserList.jsx
@@ -1,5 +1,38 @@
+import axios from "axios";
+import { Table } from "antd";
+import { useState } from "react";
+import { useEffect } from "react";
+
 const UserList = () => {
-  return <h1>Here is the list of users!</h1>;
+  const [emails, setEmails] = useState([]);
+
+  const fetchEmails = () => {
+    axios.get("http://localhost:3000/user-list").then((response) => {
+      const emailsWithKeys = response.data.map((email, index) => ({
+        key: index + 1,
+        email: email,
+      }));
+      setEmails(emailsWithKeys);
+    });
+  };
+
+  const columns = [
+    {
+      title: "Käyttäjätunnus",
+      dataIndex: "email",
+      key: "email",
+    },
+  ];
+
+  useEffect(() => {
+    fetchEmails();
+  }, []);
+
+  return (
+    <>
+      <Table columns={columns} dataSource={emails}></Table>
+    </>
+  );
 };
 
 export default UserList;
