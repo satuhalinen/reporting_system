f594a238c80fb8fb3c434b9a2d68d29cfc8c67bc
[satuhalinen] <satuh9@gmail.com>
2024-07-31 11:16:35 +0300

    add total row for salary table

--- frontend/src/routes/Salary.jsx
+++ frontend/src/routes/Salary.jsx
@@ -147,7 +147,46 @@ const Salary = () => {
         selectedSalaryDate={selectedSalaryDate}
         fileName={fileName}
       ></CreateCsv>
-      <Table columns={columns} dataSource={tableData}></Table>
+      <Table
+        columns={columns}
+        dataSource={tableData}
+        pagination={false}
+        summary={(pageData) => {
+          const totals = {};
+
+          for (const item of pageData) {
+            for (const [key, value] of Object.entries(item)) {
+              if (typeof value === "number") {
+                if (!totals[key]) {
+                  totals[key] = 0;
+                }
+                totals[key] += value;
+              }
+            }
+          }
+
+          return (
+            <Table.Summary.Row>
+              {columns.map((column) => (
+                <Table.Summary.Cell
+                  key={column.key}
+                  align={
+                    column.key === "Sukunimi" || column.key === "Etunimi"
+                      ? "left"
+                      : "right"
+                  }
+                >
+                  {column.key === "Sukunimi"
+                    ? "Kaikki yhteensä"
+                    : column.key === "Etunimi"
+                    ? ""
+                    : totals[column.key]}
+                </Table.Summary.Cell>
+              ))}
+            </Table.Summary.Row>
+          );
+        }}
+      ></Table>
     </>
   );
 };
