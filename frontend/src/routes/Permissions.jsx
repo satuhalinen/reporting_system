9026c8a0b46b1946fd2c16a6d80d2eaeec539f12
[satuhalinen] <satuh9@gmail.com>
2024-08-08 11:44:58 +0300

    add checkboxes and title components to permissions page

--- frontend/src/routes/Permissions.jsx
+++ frontend/src/routes/Permissions.jsx
@@ -1,4 +1,4 @@
-import { Form, Input, Typography } from "antd";
+import { Form, Input, Typography, Checkbox, Col, Row } from "antd";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect } from "react";
@@ -26,34 +26,72 @@ const Permissions = () => {
     fetchNamesAndEmail();
   }, []);
 
+  const onChange = (checkedValues) => {
+    console.log("checked = ", checkedValues);
+  };
+
   return (
-    <>
-      <Form
-        name="basic"
-        labelCol={{
-          span: 8,
-        }}
-        wrapperCol={{
-          span: 16,
-        }}
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      style={{
+        maxWidth: 600,
+      }}
+      autoComplete="off"
+      form={form}
+    >
+      <Title>Käyttöoikeudet raportteihin</Title>
+      <Typography.Title
+        level={2}
         style={{
-          maxWidth: 600,
+          marginLeft: "200px",
         }}
-        autoComplete="off"
-        form={form}
       >
-        <Title>Käyttöoikeudet</Title>
-        <Form.Item label="Sukunimi" name="lastname">
-          <Input disabled />
-        </Form.Item>
-        <Form.Item label="Etunimi" name="firstname">
-          <Input disabled />
-        </Form.Item>
-        <Form.Item label="Sähköpostiosoite" name="email">
-          <Input disabled />
-        </Form.Item>
-      </Form>
-    </>
+        Käyttäjän tiedot
+      </Typography.Title>
+      <Form.Item label="Sukunimi" name="lastname">
+        <Input disabled />
+      </Form.Item>
+      <Form.Item label="Etunimi" name="firstname">
+        <Input disabled />
+      </Form.Item>
+      <Form.Item label="Sähköpostiosoite" name="email">
+        <Input disabled />
+      </Form.Item>
+      <Form.Item style={{ marginLeft: "200px" }}>
+        <Typography.Title level={2}>
+          Käyttäjän oikeudet raportteihin
+        </Typography.Title>
+        <Checkbox.Group
+          style={{
+            width: "100%",
+          }}
+          onChange={onChange}
+        >
+          <Row>
+            <Col span={24}>
+              <Checkbox value="A">
+                Kaikki tunnit kumulatiivinen raportti
+              </Checkbox>
+            </Col>
+            <Col span={24}>
+              <Checkbox value="B">Kaikki tunnit raportti</Checkbox>
+            </Col>
+            <Col span={24}>
+              <Checkbox value="C">Laskutettavat tunnit raportti</Checkbox>
+            </Col>
+            <Col span={24}>
+              <Checkbox value="D">Palkkaraportti</Checkbox>
+            </Col>
+          </Row>
+        </Checkbox.Group>
+      </Form.Item>
+    </Form>
   );
 };
 
